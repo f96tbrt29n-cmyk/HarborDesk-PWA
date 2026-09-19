0ff34af03d5e5bf53299f7fb1c5a5735bbbbba09
f96tbrt29n-cmyk <f96tbrt29n@privaterelay.appleid.com>
2026-09-20 00:38:49 +0900

    Set HarborDesk updater to 1.0.218

--- update-manager.js
+++ update-manager.js
@@ -1,5 +1,5 @@
-const HD_APP_VERSION='1.0.217';
-const HD_APP_BUILD=217;
+const HD_APP_VERSION='1.0.218';
+const HD_APP_BUILD=218;
 window.HD_MODULE_STATUS=window.HD_MODULE_STATUS||{};
 window.HD_SERVICE_WORKER_STATUS='idle';
 
