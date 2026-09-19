5a7ad80e533699d4d76974de52fffc4662482f85
f96tbrt29n-cmyk <f96tbrt29n@privaterelay.appleid.com>
2026-09-19 22:51:28 +0900

    Bump HarborDesk Userscript to 1.0.5

--- HarborDesk-Kancolle.meta.js
+++ HarborDesk-Kancolle.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HarborDesk 艦これ連携
 // @namespace    https://f96tbrt29n-cmyk.github.io/HarborDesk-PWA/
-// @version      1.0.4
+// @version      1.0.5
 // @description  艦これの対応APIレスポンスを端末内で抽出し、HarborDeskへ送る。
 // @match        http://*.dmm.com/*
 // @match        https://*.dmm.com/*
