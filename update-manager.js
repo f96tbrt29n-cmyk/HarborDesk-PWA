c0dd6df6be7677e5b3aeeb2d5c984e89e9443aa2
f96tbrt29n-cmyk <f96tbrt29n@privaterelay.appleid.com>
2026-09-19 11:35:23 +0900

    Bump HarborDesk runtime to 1.0.115

--- update-manager.js
+++ update-manager.js
@@ -1,5 +1,5 @@
-const HD_APP_VERSION='1.0.114';
-const HD_APP_BUILD=114;
+const HD_APP_VERSION='1.0.115';
+const HD_APP_BUILD=115;
 window.HD_MODULE_STATUS=window.HD_MODULE_STATUS||{};
 window.HD_SERVICE_WORKER_STATUS='idle';
 
