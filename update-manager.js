f92691b62edd2c15482ebcb52b41fee99dd09056
f96tbrt29n-cmyk <f96tbrt29n@privaterelay.appleid.com>
2026-09-21 00:13:16 +0900

    Set update manager to 1.0.374

--- update-manager.js
+++ update-manager.js
@@ -1,5 +1,5 @@
-const HD_APP_VERSION='1.0.373';
-const HD_APP_BUILD=373;
+const HD_APP_VERSION='1.0.374';
+const HD_APP_BUILD=374;
 const HD_UPDATE_SNOOZE_KEY='harbordesk-update-snooze-v1';
 window.HD_MODULE_STATUS=window.HD_MODULE_STATUS||{};
 window.HD_SERVICE_WORKER_STATUS='idle';
