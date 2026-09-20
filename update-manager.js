14302755fe9001b9f42d9a2183c5ca761fc17921
f96tbrt29n-cmyk <f96tbrt29n@privaterelay.appleid.com>
2026-09-20 18:17:11 +0900

    Release HarborDesk 1.0.335 iPhone map cache fix

--- update-manager.js
+++ update-manager.js
@@ -1,5 +1,5 @@
-const HD_APP_VERSION='1.0.334';
-const HD_APP_BUILD=334;
+const HD_APP_VERSION='1.0.335';
+const HD_APP_BUILD=335;
 const HD_UPDATE_SNOOZE_KEY='harbordesk-update-snooze-v1';
 window.HD_MODULE_STATUS=window.HD_MODULE_STATUS||{};
 window.HD_SERVICE_WORKER_STATUS='idle';
