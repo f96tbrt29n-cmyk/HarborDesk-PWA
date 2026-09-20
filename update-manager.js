56d275693b59eacc763215d57dd14d3021f19e3a
f96tbrt29n-cmyk <f96tbrt29n@privaterelay.appleid.com>
2026-09-20 19:58:44 +0900

    Release HarborDesk 1.0.340 foundation hardening

--- update-manager.js
+++ update-manager.js
@@ -1,5 +1,5 @@
-const HD_APP_VERSION='1.0.339';
-const HD_APP_BUILD=339;
+const HD_APP_VERSION='1.0.340';
+const HD_APP_BUILD=340;
 const HD_UPDATE_SNOOZE_KEY='harbordesk-update-snooze-v1';
 window.HD_MODULE_STATUS=window.HD_MODULE_STATUS||{};
 window.HD_SERVICE_WORKER_STATUS='idle';
