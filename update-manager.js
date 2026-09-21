655846191bebdf708b4b256298ac167736732fa5
f96tbrt29n-cmyk <f96tbrt29n@privaterelay.appleid.com>
2026-09-22 04:32:45 +0900

    Bump update manager to 1.0.421

--- update-manager.js
+++ update-manager.js
@@ -1,5 +1,5 @@
-const HD_APP_VERSION='1.0.420';
-const HD_APP_BUILD=420;
+const HD_APP_VERSION='1.0.421';
+const HD_APP_BUILD=421;
 const HD_UPDATE_SNOOZE_KEY='harbordesk-update-snooze-v1';
 window.HD_MODULE_STATUS=window.HD_MODULE_STATUS||{};
 window.HD_SERVICE_WORKER_STATUS='idle';
