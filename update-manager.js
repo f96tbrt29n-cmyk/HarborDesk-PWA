7ebfa8aab6155d43bf5331382b7d45d237075f34
f96tbrt29n-cmyk <f96tbrt29n@privaterelay.appleid.com>
2026-09-23 13:49:44 +0900

    Bump updater to HarborDesk 1.0.437

--- update-manager.js
+++ update-manager.js
@@ -1,5 +1,5 @@
-const HD_APP_VERSION='1.0.436';
-const HD_APP_BUILD=436;
+const HD_APP_VERSION='1.0.437';
+const HD_APP_BUILD=437;
 const HD_UPDATE_SNOOZE_KEY='harbordesk-update-snooze-v1';
 window.HD_MODULE_STATUS=window.HD_MODULE_STATUS||{};
 window.HD_SERVICE_WORKER_STATUS='idle';
